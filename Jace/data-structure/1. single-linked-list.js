df397258c85b5db54558bdbe02c8a25bdc99d6b5
jaceLee2619 <jacelee2619@gmail.com>
2021-08-29 22:41:22 +0900

    Refactor single linked list by Jace

--- Jace/data-structure/1. single-linked-list.js
+++ Jace/data-structure/1. single-linked-list.js
@@ -1,129 +1,201 @@
 class SingleLinkedList {
 	constructor() {
 		this.head = null
+		this.tail = null
 		this.size = 0
 	}
 
-	// 맨 끝에 노드를 붙이는 메소드
-	append(element) {
+	// 맨 뒤에 노드를 추가하는 메소드
+	push(element) {
 		const node = new Node(element)
-		let current
 
 		if (!this.head) {
 			this.head = node
+			this.tail = node
 		} else {
+			this.tail.next = node
+			this.tail = node
+		}
+
+		++this.size
+
+		return this
+	}
+
+	// 맨 뒤의 노드를 제거하는 메소드
+	pop() {
+		if (!this.head) {
+			return
+		}
+
+		let current
+		let previous
+		let last
+
+		if (this.size === 1) {
 			current = this.head
+			this.head = null
+			this.tail = null
+		} else {
+			previous = this.head
+			current = previous.next
 
 			while (current.next) {
 				current = current.next
+				previous = previous.next
 			}
 
-			current.next = node
-		}
-
-		++this.size
-	}
-
-	// 특정 위치에 노드를 붙이는 메소드
-	insertAt(position, element) {
-		if (position < 0 || position > this.size) {
-			return
-		}
-
-		const node = new Node(element)
-		let current = this.head
-		let previous = 0
-		let count = 0
-
-		if (position === 0) {
-			node.next = current
-			this.head = node
-		} else {
-			while (count < position) {
-				++count
-				previous = current
-				current = current.next
-			}
-
-			previous.next = node
-			node.next = current
-		}
-
-		++this.size
-	}
-
-	// 특정 위치의 노드를 없애는 함수
-	removeAt(position) {
-		if (position < 0 || position > this.size) {
-			return
-		}
-
-		let current = this.head
-		let previous = 0
-		let count = 0
-
-		if (position === 0) {
-			this.head = current.next
-		} else {
-			while (count < position) {
-				++count
-				previous = current
-				current = current.next
-			}
-			previous.next = current.next
+			last = current
+			previous.next = null
+			this.tail = previous
 		}
 
 		--this.size
+
+		return last
 	}
 
-	// 특정 요소의 인덱스를 구하는 메소드
-	indexOf(element) {
+	// 맨 앞에 노드를 추가하는 메소드
+	unshift(element) {
+		const node = new Node(element)
+
+		if (!this.head) {
+			this.head = node
+			this.tail = node
+		} else {
+			node.next = this.head
+			this.head = node
+		}
+
+		++this.size
+
+		return this
+	}
+
+	// 맨 앞의 노드를 제거하는 메소드
+	shift() {
+		if (!this.head) {
+			return
+		}
+
 		let current = this.head
-		let index = 0
+
+		if (this.size === 1) {
+			this.head = null
+			this.tail = null
+		} else {
+			this.head = this.head.next
+		}
+
+		--this.size
+
+		current.next = null
+
+		return current
+	}
+
+	// 인덱스로 노드를 가져오는 메소드
+	indexOf(index) {
+		if (index < 0 || index > this.size) {
+			return
+		}
+
+		let current = this.head
+
+		for (let i = 0; i < index; ++i) {
+			current = current.next
+		}
+
+		return current
+	}
+
+	// 해당 인덱스에 노드를 추가하는 메소드
+	insertAt(index, element) {
+		if (index < 0 || index > this.size) {
+			return
+		}
+
+		if (index === 0) {
+			return this.unshift(element)
+		}
+
+		if (index === this.size) {
+			return this.push(element)
+		}
+
+		const node = new Node(element)
+		const prev = this.indexOf(index - 1)
+		const next = prev.next
+
+		prev.next = node
+		node.next = next
+
+		++this.size
+
+		return this
+	}
+
+	// 해당 인덱스의 노드를 제거하는 메소드
+	removeAt(index) {
+		if (index < 0 || index >= this.size) {
+			return
+		}
+
+		if (index === 0) {
+			return this.shift()
+		}
+
+		if (index === this.size - 1) {
+			return this.pop()
+		}
+
+		const prev = this.indexOf(index - 1)
+		const current = prev.next
+
+		prev.next = current.next
+
+		--this.size
+		current.next = null
+
+		return current
+	}
+
+	// 리스트에 해당 값의 엘리먼트가 있는지 확인하는 메소드
+	include(element) {
+		if (!this.head) {
+			return false
+		}
+
+		let current = this.head
 
 		while (current) {
 			if (current.element === element) {
-				return index
+				return true
 			}
-
-			++index
 			current = current.next
 		}
 
-		return -1
+		return false
 	}
 
-	// 특정 요소를 지우는 메소드
-	remove(element) {
-		return this.removeAt(this.indexOf(element))
-	}
-
-	// 현재 링크드 리스트의 요소 개수를 반환하는 메소드
-	length() {
-		return this.size
-	}
-
-	// 현재 링크드 리스트가 비어있는지 확인하는 메소드
-	isEmpty() {
-		return this.size ? false : true
-	}
-
-	printAll() {
+	// 리스트 안의 노드들을 전부 출력하는 메소드
+	print() {
 		let current = this.head
-		let list = ''
+		let str = ''
 
 		while (current) {
-			list += `${current.element} - `
+			str += `${current.element} - `
+
 			current = current.next
 		}
 
-		return list
+		return str
 	}
 }
 
 class Node {
-	constructor(element, next = null) {
+	constructor(element) {
 		this.element = element
-		this.next = next
+		this.next = null
 	}
 }
