96fbe4de9cdbd7648ee936ad1fc1aaaacbe5e8cb
jaceLee2619 <jacelee2619@gmail.com>
2021-09-04 02:28:28 +0900

    Complete queue by Jace

--- Jace/data-structure/4. queue.js
+++ Jace/data-structure/4. queue.js
@@ -1,19 +1,19 @@
-class Stack {
+class Queue {
 	constructor() {
 		this.head = null
 		this.tail = null
 		this.size = 0
 	}
 
-	// 노드를 추가하는 메소드
-	push(element) {
+	// 큐에 노드를 추가하는 메소드
+	enqueue(element) {
 		const node = new Node(element)
 
 		if (!this.head) {
 			this.head = node
 			this.tail = node
 		} else {
-			node.next = this.head
+			this.head.next = node
 			this.head = node
 		}
 
@@ -22,48 +22,30 @@ class Stack {
 		return this
 	}
 
-	pop() {
+	// 큐에서 노드를 내보내는 메소드
+	dequeue() {
 		if (!this.tail) {
 			return
 		}
 
-		const first = this.head
+		const last = this.tail
 
 		if (this.size === 1) {
 			this.head = null
 			this.tail = null
 		} else {
-			this.head = first.next
-			first.next = null
+			this.tail = last.next
+			last.next = null
 		}
 
 		--this.size
 
-		return first
+		return last
 	}
 
-	// 스택에 해당 값의 엘리먼트가 있는지 확인하는 메소드
-	include(element) {
-		if (!this.head) {
-			return false
-		}
-
-		let current = this.head
-
-		while (current) {
-			if (current.element === element) {
-				return true
-			}
-
-			current = current.next
-		}
-
-		return false
-	}
-
-	// 스택 안의 노드들을 전부 출력하는 메소드
+	// 큐 안의 노드들을 전부 출력하는 메소드
 	print() {
-		let current = this.head
+		let current = this.tail
 		let str = ''
 
 		while (current) {
